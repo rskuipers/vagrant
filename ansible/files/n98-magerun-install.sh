a5ca31a33c8827fdcb15a61d3b8c0356418b4020
Rick Kuipers <rick93@gmail.com>
2014-05-08 09:05:26 +0200

    Initial commit

---
+++ ansible/files/n98-magerun-install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://raw.github.com/netz98/n98-magerun/master/n98-magerun.phar -O ~/n98-magerun.phar
+mv ~/n98-magerun.phar /usr/local/bin/n98-magerun
+chmod +x /usr/local/bin/n98-magerun
